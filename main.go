0ab9a5562f9fc0ca7b6a77c8dfb23c81ae28c2aa
David Schott <david.a.schott@outlook.com>
2020-12-10 23:10:43 -0800

    deploy to heroku

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/DavidSchott/chitchat/handler"
@@ -14,19 +15,34 @@ func main() {
 	staticDir := "/static/"
 	handler.Mux.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir(handler.Config.Static))))
 
+	// Determine address to listen on
+	var address string
+	port := os.Getenv("PORT")
+	// If port is not set, Heroku is not being used
+	if port == "" {
+		port = "80"
+		address = handler.Config.Address
+	} else {
+		// If port is set, Heroku is being used
+		address = "0.0.0.0:" + port
+	}
+
 	// starting up the server
 	server := &http.Server{
-		Addr:           handler.Config.Address,
+		Addr:           address,
 		Handler:        handler.Mux,
 		ReadTimeout:    time.Duration(handler.Config.ReadTimeout * int64(time.Second)),
 		WriteTimeout:   time.Duration(handler.Config.WriteTimeout * int64(time.Second)),
 		MaxHeaderBytes: 1 << 20,
 	}
-	fmt.Println("ChitChat", version(), "started at", handler.Config.Address)
-	//server.ListenAndServe()
-	if err := server.ListenAndServeTLS("gencert/cert.pem", "gencert/key.pem"); err != nil {
+	fmt.Println("ChitChat", version(), "started at", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Error starting server", err.Error())
 	}
+	/* TLS is already enabled on PaaS platform, so this is commented out:
+	if err := server.ListenAndServeTLS("gencert/cert.pem", "gencert/key.pem"); err != nil {
+		fmt.Println("Error starting server", err.Error())
+	}*/
 }
 
 // version
